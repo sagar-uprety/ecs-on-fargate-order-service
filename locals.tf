293cec55cb6cd65d4b177c93680e1f0e1261a642
roshanramangiri <roshanramangiri@gmail.com>
2023-09-20 19:08:57 +0545

    refactoring done, default value command added

--- locals.tf
+++ locals.tf
@@ -1,10 +1,9 @@
+################################################################################
+# Input local variables
+################################################################################
+
 locals {
   name           = "order_service"
-  project        = "ecs-module-lms"
   container_name = "order"
   container_port = 3002
-  tags = {
-    Name    = local.name,
-    Project = local.project
-  }
 }
--- order.tfvars
+++
@@ -1,8 +0,0 @@
-service        = "order"
-container_name = "order"
-container_port = "3002"
-imageurl       = "426857564226.dkr.ecr.us-east-2.amazonaws.com/lms-order-ms:latest"
-environment    = "dev"
-application    = "order-service"
-owner          = "adex-lms"
-region         = "us-east-2"
